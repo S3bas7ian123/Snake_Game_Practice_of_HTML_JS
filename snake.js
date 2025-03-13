99683af69eca2dfe5326a7ade5e2f99fd72e981b
Sebastian <enzorossi@gmail.com>
2025-03-13 12:25:23 +0100

    Repeat the code...

--- snake.js
+++ snake.js
@@ -25,7 +25,7 @@ let snake = [
     }
     // (by default our snake 'll start whit 5 squares thanks this 5 coordinates)
 ]
-// snake move horizontally on x axis to the right 
+
 let snakeToRight = [
     {
         x: 160,
@@ -49,13 +49,12 @@ let snakeToRight = [
     }
 ]
 
-let dx = 10; // Horizontal movement of 10 pixels to the right
-let dy = 0; // Vertical movement (initially 0)
+let dx = 0; // Initialise to 0 to avoid initial movement
+let dy = 0; 
 
 // canvas styles: 
 game.style.backgroundColor = ' #D5E5D5';
 game.style.border = "2px solid #ADB2D4";
-console.log(game);
 
 // we need a function to draw different parts of the "snake's body"
 function drawSnakeParts(snakeBody) {
@@ -80,50 +79,7 @@ function runRightSnake() {
     snake.pop(); // remove the last element of snake using pop
 }
 
-// new function to manage keyboard input
-function isOppositeDirection(newDx, newDy) {
-   return dx === -newDx && dy === -newDy;
-}
-
-// changeDirection function, whit switch case logic 
-function changeDirection(e) {
-    // create constants for keyboard arrows (this numbers are  defined by standards dating back to the origins of computing)
-
-    const LEFT_KEY = 37; // left arrow ...
-    const RIGHT_KEY = 39; // right arrow ...
-    const UP_KEY = 38; // up arrow ...
-    const DOWN_KEY = 40; // down arrow ...
-
-    switch (e.keyCode) {
-        case LEFT_KEY:
-            if(!isOppositeDirection(-10, 0)) {
-                dx = -10;
-                dy = 0;
-            }
-            break;
-        case RIGHT_KEY:
-            if(!isOppositeDirection(10, 0)) {
-                dx = 10;
-                dy = 0;
-            }
-            break;
-        case UP_KEY:
-            if(!isOppositeDirection(0, -10)) {
-                dx = 0;
-                dy = -10
-            }
-            break;
-        case DOWN_KEY:
-            if(!isOppositeDirection(0, 10)) {
-                dx = 0;
-                dy = 10;
-            }
-            break;
-    }
-}
-// add an event listener for the keyboards events
-document.addEventListener('keydown', changeDirection);
 
 // call functions:
 drawSnakeOnScreen();
-runRightSnake();
+
